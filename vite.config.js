25e9aceff33e2581300260906d605c1375691a01
Chinmoy Bora <chinmoyborah100@gmail.com>
2024-11-07 15:39:08 +0530

    small changes

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,11 @@ export default defineConfig({
     },
   },
   preview: {
-    port: 8080,
+    port: 8081,
     strictPort: true,
    },
    server: {
-    port: 8080,
+    port: 8081,
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:8080",
